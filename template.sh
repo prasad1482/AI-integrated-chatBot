9902f54e7b51b3921bc92aca0c27d322c2e83def
unknown <djnanasatyaprasad@gmail.comgit config --global user.name>
2025-07-25 12:19:55 +0530

    This is project template

---
+++ template.sh
@@ -0,0 +1,16 @@
+# creating directories for project structure
+mkdir -p scr
+mkdir -p research
+
+#creating files for project structure
+touch scr/__init__.py
+touch scr/helper.py
+touch scr/prompt.py
+touch .env
+touch setup.py
+touch app.py
+touch research/trails.ipynb
+touch requirements.txt
+touch .gitignore
+
+echo "Directories and files created successfully."
