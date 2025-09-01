88ed3e0de053601d1a630de6314fda533f39a021
prasad1482 <djnanasatyaprasad@gmail.com>
2025-09-01 20:24:11 +0530

    Merge branch 'main' of https://github.com/prasad1482/AI-integrated-chatBot

---
+++ scr/helper.py
@@ -0,0 +1,19 @@
+
+from langchain.document_loaders import DirectoryLoader, PyPDFLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.embeddings import OpenAIEmbeddings
+from typing import List
+from langchain.schema import Document 
+
+# Extract text from PDF files
+def load_pdf_files(data):
+    loader = DirectoryLoader(
+        data,
+        glob="*.pdf",
+        loader_cls=PyPDFLoader
+    )
+
+    documents = loader.load()
+    return documents
+
+
